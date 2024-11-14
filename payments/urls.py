d9979668fe2bf2e3a20d35e370000526da930d42
oerijogoo <51825282+oerijogoo@users.noreply.github.com>
2024-11-14 05:29:42 +0000

    static

--- payments/urls.py
+++ payments/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings 
-from django.conf.urls.static import static   
+from django.conf.urls.static import static  
+ 
 
 
 urlpatterns = [
@@ -27,4 +28,5 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
