0689556856f6809dfb267d207cc7f4a2e89e28f3
oerijogoo <51825282+oerijogoo@users.noreply.github.com>
2024-11-16 16:56:03 +0000

    model

--- payment/models.py
+++ payment/models.py
@@ -23,3 +23,14 @@ class ShippingAddress(models.Model):
     def __str__(self):
 
         return 'Shipping Address - '+ str(self.id)
+    
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    shipping_address = models.TextField(max_length=1000)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2) 
+    date_orderd = models.DateTimeField(auto_now_add=True)
+
+    user= models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
