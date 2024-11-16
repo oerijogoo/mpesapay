582b78c582c9cf82e7f26e18b68ed8bd3ecf5624
oerijogoo <51825282+oerijogoo@users.noreply.github.com>
2024-11-16 17:24:32 +0000

    chec out

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from store.models import Product
+
 
 # Create your models here.
 
@@ -33,4 +35,26 @@ class Order(models.Model):
     date_orderd = models.DateTimeField(auto_now_add=True)
 
     user= models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order -#'+ str(self.id)
+
+class OrderItem(models.Model):
+    # ForeignKey to Order
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    
+    # ForeignKey to Product
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    # ForeignKey to User (optional)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order -'+ str(self.id)
+
+
+        
 
