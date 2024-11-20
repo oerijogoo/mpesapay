c3c68d87879cedc277255208038220d26c167e31
django_api <joerironald@gmail.com>
2024-11-20 11:19:05 +0300

    footer

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,11 @@ setTimeout(function(){
     message_timer.style.display = "none"
 
 },3000)
+
+// Update copyright year
+    const currentYearElement = document.getElementById('current-year');
+    if (currentYearElement) {
+        currentYearElement.textContent = new Date().getFullYear();
+    } else {
+        console.error('Current year element not found');
+    }
