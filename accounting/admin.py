d30cc6d4ffca8842888d2009ff33bec28ab62c17
django_api <joerironald@gmail.com>
2024-12-09 11:58:45 +0300

    cooperative

---
+++ accounting/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import (IncomeType,Income,
+                    ExpenseType,Expense)
+
+# Register your models here.
+
+admin.site.register(IncomeType)
+admin.site.register(Income)
+admin.site.register(ExpenseType)
+admin.site.register(Expense)
---
+++ shares/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import (ShareAccount,ShareBuy,
+        ShareSell,)
+
+# Register your models here.
+
+admin.site.register(ShareAccount)
+admin.site.register(ShareBuy)
+admin.site.register(ShareSell)
+
+
