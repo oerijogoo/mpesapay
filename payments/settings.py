3d863c399e56a98cd76a076e86fcf0374e92c336
django_api <joerironald@gmail.com>
2025-06-09 22:11:42 +0300

    first commit

--- payments/settings.py
+++ payments/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-sa)ik2hslcintl4(r)_-u@nsq1m%xw16@o_ye2i34&ze^(c2#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['oerimpesa.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['joelandscappers.onrender.com', 'localhost', '127.0.0.1']
 
 
 
